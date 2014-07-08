ddd2e58fc3522f3eaa207345eba5cabf50d25019
John Spray <jspray@redhat.com>
2014-07-08 19:00:20 +0100

    qa: update kclient layout test for newfs changes
    Get all pool IDs at runtime, reduce assumption
    to assuming the name of the default data pool
    is 'data'.
    
    Because the `cephfs` tool otherwise assumes layout
    pool ID to be zero, also include explicit -p arg
    to all set_layout calls.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- qa/workunits/kclient/file_layout.sh
+++ qa/workunits/kclient/file_layout.sh
@@ -8,19 +8,18 @@ rm new_layout || true
 rm file2_layout || true
 rm temp || true
 
-echo "layout.data_pool:     0
+# Assume default data pool called 'data' as it is by teuthology ceph task
+data_pool_id=`ceph osd dump | sed -n "s/^pool \([0-9]*\) 'data'.*/\1/p"`
+
+echo "layout.data_pool:     $data_pool_id
 layout.object_size:   1048576
 layout.stripe_unit:   1048576
 layout.stripe_count:  1" > new_layout
-echo "layout.data_pool:     0
+echo "layout.data_pool:     $data_pool_id
 layout.object_size:   2097152
 layout.stripe_unit:   1048576
 layout.stripe_count:  2" > file2_layout
-echo "layout.data_pool:     3
-layout.object_size:   1048576
-layout.stripe_unit:   1048576
-layout.stripe_count:  1" > file3_layout
-echo "layout.data_pool:     0
+echo "layout.data_pool:     $data_pool_id
 layout.object_size:   1048576
 layout.stripe_unit:   262144
 layout.stripe_count:  1" > file4_layout
@@ -28,7 +27,7 @@ layout.stripe_count:  1" > file4_layout
 
 mkdir layout_test
 cephfs layout_test show_layout
-cephfs layout_test set_layout -u $MB -c 1 -s $MB
+cephfs layout_test set_layout -u $MB -c 1 -s $MB -p $data_pool_id
 touch layout_test/file1
 cephfs layout_test/file1 show_layout > temp
 diff new_layout temp || return 1
@@ -38,7 +37,7 @@ diff new_layout temp || return 1
 touch layout_test/file2
 cephfs layout_test/file2 show_layout > temp
 diff new_layout temp || return 1
-cephfs layout_test/file2 set_layout -u $MB -c 2 -s $twoMB
+cephfs layout_test/file2 set_layout -u $MB -c 2 -s $twoMB -p $data_pool_id
 cephfs layout_test/file2 show_layout > temp
 diff file2_layout temp || return 1
 
@@ -52,7 +51,9 @@ sudo ls /sys/kernel/debug/ceph/\* || true
 sudo bash -c 'ls /sys/kernel/debug/ceph/*' || true
 sudo bash -c 'cat /sys/kernel/debug/ceph/*/mdsmap' > temp
 ceph osd pool create newpool 2 || true
-ceph mds add_data_pool 3 || true
+newpool_id=`ceph osd dump | sed -n "s/^pool \([0-9]*\) 'newpool'.*/\1/p"`
+
+ceph mds add_data_pool $newpool_id || true
 sudo bash -c 'cat /sys/kernel/debug/ceph/*/mdsmap' > temp2
 while diff -q temp2 temp
 do
@@ -62,7 +63,12 @@ do
 done
 sudo rm temp temp2
 
-cephfs layout_test/file3 set_layout -p 3
+echo "layout.data_pool:     $newpool_id
+layout.object_size:   1048576
+layout.stripe_unit:   1048576
+layout.stripe_count:  1" > file3_layout
+
+cephfs layout_test/file3 set_layout -p $newpool_id
 cephfs layout_test/file3 show_layout > temp
 diff file3_layout temp || return 1
 echo "hello, I'm a file in pool3" > layout_test/file3
@@ -70,7 +76,7 @@ echo "hello, I'm a file in pool3" > layout_test/file3
 touch layout_test/file4
 cephfs layout_test/file4 show_layout > temp
 diff new_layout temp || return 1
-cephfs layout_test/file4 set_layout -u 262144
+cephfs layout_test/file4 set_layout -p $data_pool_id -u 262144
 cephfs layout_test/file4 show_layout > temp
 diff file4_layout temp || return 1
 echo "hello, I'm a file with a small stripe unit!" > layout_test/file3
