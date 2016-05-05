ef7d8015d88438b4a223afbd29086ab5121c3e43
Piotr Dałek <git@predictor.org.pl>
2016-05-05 21:03:37 +0200

    msg/simple: add ms_handle_refused callback
    Added new callback (ms_handle_refused) to dispatchers. It is called
    once connection attempt fails with ECONNREFUSED.
    Also added dummy ms_handle_refused handlers across codebase.
    
    Signed-off-by: Piotr Dałek <git@predictor.org.pl>
    (cherry picked from commit d58d7d3dbd21a8dca0a19964f51cb9bf78814a75)

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -4736,6 +4736,11 @@ bool OSD::ms_handle_reset(Connection *con)
   return true;
 }
 
+bool OSD::ms_handle_refused(Connection *con)
+{
+  return false;
+}
+
 struct C_OSD_GetVersion : public Context {
   OSD *osd;
   uint64_t oldest, newest;
