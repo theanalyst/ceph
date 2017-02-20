49132ab6aa0863c746c80e365e2cbbe553027175
Abhishek Lekshmanan <abhishek@suse.com>
2017-02-20 18:28:09 +0100

    the quintessential I've no idea what I'm doing commit

--- src/rgw/rgw_tag_s3.cc
+++ src/rgw/rgw_tag_s3.cc
@@ -8,36 +8,84 @@
 
 using namespace std;
 
-void RGWObjTags_S3::to_xml(ostream& out){
-  for (const auto& tag : tags){
-    out << "<Key>" << tag->first;
-    if (!tag->second.empty())
-      out << "<Value>" << tag->second;
+bool RGWObjTagEntry_S3::xml_end(const char* el){
+  RGWObjTagKey_S3 *key_obj = static_cast<RGWObjTagKey_S3 *>(find_first("Key"));
+  RGWObjTagValue_S3 *val_obj = static_cast<RGWObjTagValue_S3 *>(find_first("Value"));
+
+  if (!key_obj)
+    return false;
+
+  string s = key_obj->get_data();
+  if (s.empty()){
+    return false;
+  }
+
+  key = s;
+  if (val_obj) {
+    val = val_obj->get_data();
+  }
+
+  return true;
+}
+
+bool RGWObjTags_S3::xml_end(const char* el){
+  XMLObjIter iter = find("Tag");
+  RGWObjTagEntry_S3 *tag = static_cast<RGWObjTagEntry_S3 *>(iter.get_next());
+  while (tag) {
+    const string& key = tag->get_key();
+    tags[key]=tag->get_val();
+    add_tag(key,val);
+    tag = static_cast<RGWObjTagEntry_S3 *>(iter.get_next());
   }
 }
 
-void RGWObjTags_S3::dump_xml(Formatter *f) const {
-  f->open_object_section_in_ns("Tagging", XMLNS_AWS_S3);
-  f->open_section("TagSet");
-  for (const auto& tag : tags){
-    f->open_section("Tag");
-    out << "<Key>" << tag->first;
+void RGWObjTags_S3::to_xml(CephContext* cct, ostream& out){
+  for (const auto& tag: tags) {
+    out << "<Tag>";
+    out << "<Key>" << tag->first << "</Key>";
     if (!tag->second.empty())
-      out << "<Value>" << tag->second;
-    f->close_section();
+      out << "<Value>" << tag->second << "</Value>";
+    out << "</Tag>"
   }
-  f->close_section();
-  f->close_section();
 }
 
-int RGWObjTagsParser::get_obj_tags(map <string, string>& tags){
-  // validating based on http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUTtagging.html
-  XMLObj *tagging = find_first("Tagging");
-  if (!tagging)
-    return -EINVAL;
-  XMLObj *tagset = tagging->find_first("TagSet");
-  if (!tagset)
-    return -EINVAL;
+bool RGWObjTagSet_S3::xml_end(const char* el){
+  RGWObjTags_S3 *tags = static_cast<RGWObjTagSet_S3 *> (find_first_of("TagSet"));
+  return (tags != nullptr);
+}
 
+void RGWObjTagSet_S3::to_xml(CephContext* cct, ostream& out){
+  out << "<TagSet>";
 
 }
+// void RGWObjTags_S3::to_xml(ostream& out){
+//   for (const auto& tag : tags){
+//     out << "<Key>" << tag->first;
+//     if (!tag->second.empty())
+//       out << "<Value>" << tag->second;
+//   }
+// }
+
+// void RGWObjTags_S3::dump_xml(Formatter *f) const {
+//   f->open_object_section_in_ns("Tagging", XMLNS_AWS_S3);
+//   f->open_section("TagSet");
+//   for (const auto& tag : tags){
+//     f->open_section("Tag");
+//     out << "<Key>" << tag->first;
+//     if (!tag->second.empty())
+//       out << "<Value>" << tag->second;
+//     f->close_section();
+//   }
+//   f->close_section();
+//   f->close_section();
+// }
+
+// int RGWObjTagsParser::get_obj_tags(map <string, string>& tags){
+//   // validating based on http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUTtagging.html
+//   XMLObj *tagging = find_first("Tagging");
+//   if (!tagging)
+//     return -EINVAL;
+//   XMLObj *tagset = tagging->find_first("TagSet");
+//   if (!tagset)
+//     return -EINVAL;
+// }
--- src/rgw/rgw_tag_s3.h
+++ src/rgw/rgw_tag_s3.h
@@ -13,27 +13,78 @@
 
 using namespace std;
 
-class RGWObjTags_S3 : public RGWObjTags, public XMLObj
+class RGWObjTagKey_S3: public XMLObj
+{
+public:
+  RGWObjTagKey_S3() {};
+  ~RGWObjTagKey_S3() {};
+};
+
+class RGWObjTagValue_S3: public XMLObj
+{
+public:
+  RGWObjTagValue_S3() {};
+  ~RGWObjTagValue_S3() {};
+};
+
+class RGWObjTagEntry_S3: public XMLObj
+{
+  string key;
+  string val;
+public:
+  RGWObjTagEntry_S3() {}
+  RGWObjTagEntry_S3(string k,string v):key(k),val(v) {};
+  ~RGWObjTagEntry_S3() {}
+
+  bool xml_end(const char* el);
+  const string& get_key () { return key;}
+  const string& get_val () { return val;}
+  //void to_xml(CephContext *cct, ostream& out) const;
+};
+
+class RGWObjTags_S3: public RGWObjTags, public XMLObj
 {
 public:
   RGWObjTags_S3() {}
   ~RGWObjTags_S3() {}
 
-  void to_xml(ostream& out);
-  void dump_xml(Formatter *f) const;
-
+  bool xml_end(const char* el);
+  void to_xml(CephContext *cct, ostream& out) const;
 };
 
-class RGWObjTagsParser : public RGWXMLParser
+class RGWObjTagSet_S3: public XMLObj
 {
-  XMLObj *alloc_obj(const char *el){
-    return new XMLObj;
-  }
 public:
-  RGWObjTagsParser() {}
-  ~RGWObjTagsParser() {}
+  RGWObjTagSet_S3() {}
+  ~RGWObjTagSet_S3() {}
 
-  int get_obj_tags(map <string,string>& tags);
+  bool xml_end(const char* el);
+  void to_xml(CephContext *cct, ostream& out) const;
 };
+
+// class RGWObjTags_S3 : public RGWObjTags, public XMLObj
+// {
+// public:
+//   RGWObjTags_S3() {}
+//   ~RGWObjTags_S3() {}
+
+//   void to_xml(ostream& out);
+//   void dump_xml(Formatter *f) const;
+
+// };
+
+
+
+// class RGWObjTagsParser : public RGWXMLParser
+// {
+//   XMLObj *alloc_obj(const char *el){
+//     return new XMLObj;
+//   }
+// public:
+//   RGWObjTagsParser() {}
+//   ~RGWObjTagsParser() {}
+
+//   int get_obj_tags(map <string,string>& tags);
+// };
 
 #endif /* RGW_TAG_S3_H */
