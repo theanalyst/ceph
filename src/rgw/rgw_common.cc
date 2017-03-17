a1ce2741213bdf5739134c07d90eb3032a792bfa
Abhishek Lekshmanan <abhishek@suse.com>
2017-03-17 14:26:02 +0100

    rgw: move string split to rgw_common
    Since this could be reused by other classes that use a string split
    method instead of implementing again
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -1251,6 +1251,20 @@ static int parse_list_of_flags(struct rgw_name_to_flag *mapping,
   return 0;
 }
 
+static void split(const std::string &s, char delim, std::vector<std::string> &elems) {
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    elems.push_back(item);
+  }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::vector<std::string> elems;
+  split(s, delim, elems);
+  return elems;
+}
+
 static struct rgw_name_to_flag cap_names[] = { {"*",     RGW_CAP_ALL},
                   {"read",  RGW_CAP_READ},
 		  {"write", RGW_CAP_WRITE},
--- src/rgw/rgw_lc.cc
+++ src/rgw/rgw_lc.cc
@@ -198,21 +198,6 @@ bool RGWLC::if_already_run_today(time_t& start_date)
     return false;
 }
 
-static std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
-  std::stringstream ss(s);
-  std::string item;
-  while (std::getline(ss, item, delim)) {
-      elems.push_back(item);
-  }
-  return elems;
-}
-
-static std::vector<std::string> split(const std::string &s, char delim) {
-  std::vector<std::string> elems;
-  split(s, delim, elems);
-  return elems;
-}
-
 int RGWLC::bucket_lc_prepare(int index)
 {
   map<string, int > entries;
