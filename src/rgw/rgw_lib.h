02693f7239b0958981b18415b8158b5e704a24cf
Abhishek Lekshmanan <abhishek@suse.com>
2018-02-12 18:05:03 +0100

    rgw:  make init env methods return an error
    Since web frontends may signal an error when requests are malformed or so, let
    us double check this and raise errors early. The current user of this is
    civetweb frontend; which can potentially return null from `parse_http_headers`
    when a HTTP header without a ":" is supplied at which point headers.value is
    null which can lead to undefined behaviour later in RGW.
    
    Fixes: http://tracker.ceph.com/issues/23039
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>
    (cherry picked from commit 7872a831783e17dcc4d0aa70cffc256afb664f7a)
    
    Conflicts:
    -	src/rgw/rgw_asio_client.h
      keeping get_conn_close which is a part of L
    -	src/rgw/rgw_lib.h
      dropping init.env changes introduced in mimic

--- src/rgw/rgw_lib.h
+++ src/rgw/rgw_lib.h
@@ -63,7 +63,9 @@ namespace rgw {
     RGWLibIO(const RGWUserInfo &_user_info)
       : user_info(_user_info) {}
 
-    void init_env(CephContext *cct) override {}
+    int init_env(CephContext *cct) override {
+      return 0;
+    }
 
     const RGWUserInfo& get_user() {
       return user_info;
