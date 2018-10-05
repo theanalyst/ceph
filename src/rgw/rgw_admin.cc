606cb45cfd2a545151003098470d61a3d1e45ebe
Abhishek Lekshmanan <abhishek@suse.com>
2018-10-05 18:38:17 +0200

    rgw_admin: bi purge forces index removal on force flag
    To remove orphaned indexes, ignore the init_bucket output and purge the index
    anyway on force even if init_bucket doesn't return which can happen when the
    bucket is removed but index objects are still lying around. A todo to tackle is
    also remove the instance metadata at the end
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -5921,12 +5921,16 @@ next:
     RGWBucketInfo cur_bucket_info;
     rgw_bucket cur_bucket;
     ret = init_bucket(tenant, bucket_name, string(), cur_bucket_info, cur_bucket);
+    // if force is passed, assume user is passing a stale index so bi maybe invalid
     if (ret < 0) {
-      cerr << "ERROR: could not init current bucket info for bucket_name=" << bucket_name << ": " << cpp_strerror(-ret) << std::endl;
-      return -ret;
-    }
-
-    if (cur_bucket_info.bucket.bucket_id == bucket_info.bucket.bucket_id && !yes_i_really_mean_it) {
+      if (!yes_i_really_mean_it){
+        cerr << "ERROR: could not init current bucket info for bucket_name=" << bucket_name << ": " << cpp_strerror(-ret) << std::endl;
+        cerr << "if you're really really sure the index is stale pass --yes-i-really-mean-it" << std::endl;
+        return -ret;
+      }
+      cerr << "purging anyway since --yes-i-really-mean-it is passed" << std::endl;
+    } else if (cur_bucket_info.bucket.bucket_id == bucket_info.bucket.bucket_id
+               && !yes_i_really_mean_it) {
       cerr << "specified bucket instance points to a current bucket instance" << std::endl;
       cerr << "do you really mean it? (requires --yes-i-really-mean-it)" << std::endl;
       return EINVAL;
