fb825a8d1e6c93804249853bd9826ab0c407962b
Nathan Cutler <ncutler@suse.com>
2018-11-24 11:09:11 +0100

    qa: optimize scripts
    Signed-off-by: Nathan Cutler <ncutler@suse.com>
    (cherry picked from commit 762703efa33366a7263bb9b49d6a32e0af6f05c7)
    
    Conflicts:
    	qa/tasks/deepsea.py
    	qa/tasks/scripts.py
    	qa/tasks/ses_qa.py

--- qa/tasks/ses_qa.py
+++ qa/tasks/ses_qa.py
@@ -8,6 +8,7 @@ import logging
 
 from salt_manager import SaltManager
 from scripts import Scripts
+from util import introspect_roles
 
 from teuthology.exceptions import (
     ConfigError,
@@ -21,6 +22,7 @@ ses_qa_ctx = {}
 class SESQA(Task):
 
     def __init__(self, ctx, config):
+        global ses_qa_ctx
         super(SESQA, self).__init__(ctx, config)
         if ses_qa_ctx:
             self.log = ses_qa_ctx['logger_obj']
@@ -31,13 +33,16 @@ class SESQA(Task):
             self.log.debug("populating ses_qa_ctx (we are *not* in a subtask)")
             self._populate_ses_qa_context()
         self.master_remote = ses_qa_ctx['master_remote']
-        self.scripts = Scripts(self.master_remote, ses_qa_ctx['logger_obj'])
+        self.remotes = ses_qa_ctx['remotes']
+        self.scripts = Scripts(self.remotes)
         self.sm = ses_qa_ctx['salt_manager_instance']
 
     def _populate_ses_qa_context(self):
+        global ses_qa_ctx
         ses_qa_ctx['roles'] = self.ctx.config['roles']
         ses_qa_ctx['salt_manager_instance'] = SaltManager(self.ctx)
         ses_qa_ctx['master_remote'] = ses_qa_ctx['salt_manager_instance'].master_remote
+        introspect_roles(self.ctx, self.log, ses_qa_ctx, quiet=False)
 
     def os_type_and_version(self):
         os_type = self.ctx.config.get('os_type', 'unknown')
@@ -57,54 +62,16 @@ class SESQA(Task):
         super(SESQA, self).teardown()
 
 
-class MGRPlugin(SESQA):
+class Validation(SESQA):
 
-    err_prefix = "(mgr_plugin subtask) "
+    err_prefix = "(validation subtask) "
 
     def __init__(self, ctx, config):
-        ses_qa_ctx['logger_obj'] = log.getChild('mgr_plugin')
-        self.name = 'ses_qa.mgr_plugin'
-        super(MGRPlugin, self).__init__(ctx, config)
+        global ses_qa_ctx
+        ses_qa_ctx['logger_obj'] = log.getChild('validation')
+        self.name = 'ses_qa.validation'
+        super(Validation, self).__init__(ctx, config)
         self.log.debug("munged config is {}".format(self.config))
-
-    def mgr_plugin_dashboard(self, **kwargs):
-        """
-        Minimal/smoke test for the MGR dashboard plugin
-        """
-        self.scripts.mgr_dashboard_module_smoke()
-
-    def mgr_plugin_influx(self, **kwargs):
-        """
-        Minimal/smoke test for the MGR influx plugin
-
-        Tests the 'influx' MGR plugin, but only on openSUSE Leap 15.0.
-
-        Testing on SLE-15 is not currently possible because the influxdb
-        package is not built in IBS for anything higher than SLE-12-SP4.
-        Getting it to build for SLE-15 requires a newer golang stack than what
-        is available in SLE-15 - see
-        https://build.suse.de/project/show/NON_Public:infrastructure:icinga2
-        for how another team is building it (and no, we don't want to do that).
-
-        Testing on openSUSE Leap 15.0 is only possible because we are building
-        the influxdb package in filesystems:ceph:nautilus with modified project
-        metadata.
-
-        (This problem will hopefully go away when we switch to SLE-15-SP1.)
-        """
-        zypper_cmd = ("sudo zypper --non-interactive --no-gpg-check "
-                      "install --force --no-recommends {}")
-        os_type, os_version = self.os_type_and_version()
-        if os_type == 'opensuse' and os_version >= 15:
-            self.ctx.cluster.run(
-                args=zypper_cmd.format(' '.join(["python3-influxdb", "influxdb"]))
-                )
-            self.scripts.mgr_plugin_influx()
-        else:
-            self.log.warning(
-                "mgr_plugin_influx test case not implemented for OS ->{}<-"
-                .format(os_type + " " + str(os_version))
-                )
 
     def begin(self):
         self.log.debug("Processing tests: ->{}<-".format(self.config.keys()))
@@ -113,7 +80,7 @@ class MGRPlugin(SESQA):
             if not isinstance(kwargs, dict):
                 raise ConfigError(self.err_prefix + "Method config must be a dict")
             self.log.info(
-                "Running MGR plugin test {} with config ->{}<-"
+                "Running validation test {} with config ->{}<-"
                 .format(method_spec, kwargs)
                 )
             method = getattr(self, method_spec, None)
@@ -125,4 +92,4 @@ class MGRPlugin(SESQA):
 
 
 task = SESQA
-mgr_plugin = MGRPlugin
+validation = Validation
