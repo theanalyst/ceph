edfa083806478e578fa087e2646abb113b5f1b04
Abhishek Lekshmanan <abhishek@suse.com>
2019-04-09 15:11:34 +0200

    qa: rgw: scheduler use the newly set explicit rgw admin socket
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- qa/tasks/rgw_scheduler_tests.py
+++ qa/tasks/rgw_scheduler_tests.py
@@ -283,6 +283,10 @@ def task(ctx, config):
 
     rgw_scheduler_tests_conf = {}
     for client in clients:
+        cluster_name, daemon_type, client_id = teuthology.split_role(client)
+        client_with_id = daemon_type + '.' + client_id
+        client_with_cluster = cluster_name + '.' + client_with_id
+
         endpoint = ctx.rgw.role_endpoints.get(client)
         assert endpoint, 's3tests: no rgw endpoint for {}'.format(client)
         #log.debug('abhi got endpoint', endpoint)
@@ -293,6 +297,7 @@ def task(ctx, config):
                     'req_count': 100,
                     'base_url' : endpoint.url(),
                     'auth_type' : 's3',
+                    'admin_sock_path': '/var/run/ceph/rgw.{client_with_cluster}.asok'.format(client_with_cluster=client_with_cluster)
                 }
                 }
             )
