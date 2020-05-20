5615b0e4fc8a483f2df367760eb99c2babb8b82a
Abhishek Lekshmanan <abhishek@suse.com>
2020-05-20 13:44:59 +0200

    add account.cc to add compilation unit
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/rgw_account.h
+++ src/rgw/rgw_account.h
@@ -15,29 +15,77 @@
 
 #pragma once
 
+#include <atomic>
+
+#include "include/types.h"
+
+class RGWRole;
+class rgw_user;
+class optional_yield;
+
 struct AccountQuota {
   uint16_t max_users {1000};
   uint16_t max_roles {1000};
-}
 
-class RGWAccount {
+  void encode(bufferlist& bl) const {
+    ENCODE_START(1,1,bl);
+    encode(max_users, bl);
+    encode(max_roles, bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::const_iterator& bl) {
+    DECODE_START(1,bl);
+    decode(max_users, bl);
+    decode(max_roles, bl);
+    DECODE_FINISH(bl);
+  }
+};
+WRITE_CLASS_ENCODER(AccountQuota)
+
+class RGWAccountInfo {
   std::string id;
   std::string tenant;
   AccountQuota account_quota;
-public:
-  RGWAccount(std::string&& _id) : id(std::move(_id)) {}
-  RGWAccount(const std::string& _id): id(_id) {}
 
-  RGWAccount(std::string&& _id,
-	     std::string&& _tenant) : id(std::move(_id)),
-				      tenant(std::move(_tenant))
+  std::atomic<uint16_t> users_count = {0};
+  std::atomic<uint16_t> roles_count = {0};
+public:
+  explicit RGWAccountInfo(std::string&& _id) : id(std::move(_id)) {}
+  explicit RGWAccountInfo(const std::string& _id): id(_id) {}
+
+  RGWAccountInfo(const std::string& _id,
+		 const std::string& _tenant) : id(_id),
+					       tenant(_tenant)
   {}
 
-  ~RGWAccount() = default;
+  RGWAccountInfo(std::string&& _id,
+		 std::string&& _tenant) : id(std::move(_id)),
+					  tenant(std::move(_tenant))
+  {}
 
-  void encode(bufferlist& bl) const;
-  void decode(bufferlist::const_iterator& bl);
+  ~RGWAccountInfo() = default;
+
+  void encode(bufferlist& bl) const {
+    ENCODE_START(1,1,bl);
+    encode(id, bl);
+    encode(tenant, bl);
+    encode(account_quota, bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::const_iterator& bl) {
+    DECODE_START(1, bl);
+    decode(id, bl);
+    decode(tenant, bl);
+    decode(account_quota, bl);
+    DECODE_FINISH(bl);
+  }
+
+  const std::string& get_id() const { return id; }
+  const std::string& get_tenant() { return tenant; }
 };
+WRITE_CLASS_ENCODER(RGWAccountInfo)
 
 class RGWAccountCtl
 {
@@ -49,4 +97,14 @@ public:
 
   int remove_user(const rgw_user& user);
   int remove_role(const RGWRole& role);
-}
+
+  int get_info_by_id(const std::string& id,
+		     RGWAccountInfo* info,
+		     optional_yield y);
+
+  int store_info(const RGWAccountInfo& info,
+		 optional_yield y);
+
+  // TODO
+  // get_info_by_tenant()
+};
